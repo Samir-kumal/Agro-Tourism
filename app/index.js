e21fda6dda5bc64dc383fc18d3794bc77d823be1
Samir Kumal <samirkumal@2400-1A00-BDE0.ip6.wlink.com.np>
2023-11-12 17:31:37 +0545

    project initiated

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="flex items-center justify-center ">
+      <Text className="font-bold text-2xl">Hello World</Text>
+    </View>
+  );
+};
+
+export default index;
